e0e0186e8997858bb63983500ed79d07fb8625b5
singing-cat <519797921@qq.com>
2023-05-09 12:00:50 +0800

    Finished Lab01

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -3,5 +3,17 @@
 
 int ll_has_cycle(node *head) {
     /* your code here */
-    return 0;
+    if(head == NULL || head->next == NULL)
+        return 0;
+    node *tortoise = head, *hare = head;
+    int flag = 0;
+    while(hare && hare->next) {
+        hare = hare->next->next;
+        tortoise = tortoise->next;
+        if(hare == tortoise) {
+            flag = 1;
+            break;
+        }
+    }
+    return flag;
 }
