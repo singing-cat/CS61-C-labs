1c240f8e1a5e93501c09723e81a226c5204898ca
singing-cat <519797921@qq.com>
2023-05-09 18:50:33 +0800

    Finished Lab02

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -6,5 +6,11 @@
 
 void lfsr_calculate(uint16_t *reg) {
     /* YOUR CODE HERE */
+    uint16_t bit_0 = (*reg)&1;
+    uint16_t bit_2 = ((*reg)>>2)&1;
+    uint16_t bit_3 = ((*reg)>>3)&1;
+    uint16_t bit_5 = ((*reg)>>5)&1;
+    uint16_t bit_fill = bit_0^bit_2^bit_3^bit_5;
+    *reg = ((*reg)>>1)&(0b0111111111111111);
+    *reg = (*reg)^(bit_fill<<15);
 }
-
